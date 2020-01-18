d513aa893b65928f1001fd46677d038800d86da2
haruelrovix <havit@adakerja.com>
2020-01-18 19:49:10 +0700

    feat: Use DailyDeals in App.js

--- components/DailyDeals/DateList.js
+++ components/DailyDeals/DateList.js
@@ -3,7 +3,7 @@ import {View, Text} from 'react-native';
 
 const DateList = props => (
   <View>
-    <Text>This will be DateList component with {props}</Text>
+    <Text>This will be DateList component with {}</Text>
   </View>
 );
 
--- components/DailyDeals/Discount.js
+++ components/DailyDeals/Discount.js
@@ -3,7 +3,7 @@ import {View, Text} from 'react-native';
 
 const Discount = props => (
   <View>
-    <Text>This will be Discount component with {props}</Text>
+    <Text>This will be Discount component with {}</Text>
   </View>
 );
 
